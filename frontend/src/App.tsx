2fc43ad0f64e6f1d9a52099d56765896a6ad74b3
RaiWhaaa <jeng.pongsaporn@gmail.com>
2025-05-15 15:44:59 +0700

    fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 //import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import AppointmentPage from "./pages/ Appointment";
+import AppointmentPage from "./pages/Appointment";
 import MyAppointmentsPage from "./pages/MyAppointments";
 
 function App() {
